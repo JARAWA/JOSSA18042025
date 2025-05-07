410b639b2fb099e9866770584ea76d4205eaec43
JARAWA <129917912+JARAWA@users.noreply.github.com>
2025-05-07 20:32:28 +0530

    Update auth-verification.js

--- app/static/js/auth-verification.js
+++ app/static/js/auth-verification.js
@@ -25,6 +25,7 @@ const auth = getAuth(app);
 // Allowed origins
 const ALLOWED_ORIGINS = [
     'https://nextstep-nexn.onrender.com',
+    'https://nextstep-test.onrender.com',
     'http://localhost:3000',
     'http://127.0.0.1:3000'
 ];
